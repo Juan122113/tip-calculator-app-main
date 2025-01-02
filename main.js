e0c053826e0ea87ae65719dc0d456f27a82bdcf4
Juan122113 <ammiel89@hotmail.com>
2025-01-02 13:26:46 -0300

    Update index.html & creation of main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// const body = document.getElementById("body");
+
+// console.log(body);
+
+const form = document.getElementById("form");
+
+function handleSubmit(e) {
+    e.preventDefault() // prevent the default behaviour
+  }
+
+form.addEventListener('submit', handleSubmit);
