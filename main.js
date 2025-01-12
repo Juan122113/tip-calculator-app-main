cc401102f9dd00a90d83c33db0bcfdde346d9802
Juan122113 <ammiel89@hotmail.com>
2025-01-12 13:14:20 -0300

    Update main.js
    Creating variables

--- main.js
+++ main.js
@@ -3,6 +3,14 @@
 // console.log(body);
 
 const form = document.getElementById("form");
+const bill = document.getElementById("bill");
+const percent5 = document.getElementById("5-percent");
+const percent10 = document.getElementById("10-percent");
+const percent15 = document.getElementById("15-percent");
+const percent25 = document.getElementById("25-percent");
+const percent50 = document.getElementById("50-percent");
+const customInput = document.getElementById("custom-input");
+const numberOfPeople = document.getElementById("number_of_People");
 
 function handleSubmit(e) {
     e.preventDefault() // prevent the default behaviour
