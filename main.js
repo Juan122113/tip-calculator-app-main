19dadb0dc87313465ae97a0f08a1bb8a19f44fa3
Juan122113 <ammiel89@hotmail.com>
2025-02-04 08:14:58 -0300

    Update main.js
    Cleaning the code

--- main.js
+++ main.js
@@ -47,28 +47,16 @@ function puttingTotalPersonOnScreen(totalPerPersonDecimal, total) {
 
 function totalPerPersonToFixed(totalPerPerson,total) {
   const totalPerPersonDecimal = totalPerPerson.toFixed(2);
-  console.log(totalPerPersonDecimal);
   puttingTotalPersonOnScreen(totalPerPersonDecimal, total);
 }
 
 function calculatingTotalPerson(tipAmountFloat, numberOfPeople, bill, totalPerPerson, billValueNum, tipPercentage) {
   totalPerPerson = (billValueNum + tipPercentage) / numberOfPeople.value;
-  console.log(totalPerPerson);
-  console.log(typeof bill);
-  console.log(typeof tipPercentage);
-  console.log(tipPercentage);
-  console.log(typeof billValueFloat);
-  console.log(typeof tipAmountFloat);
-  // console.log(billValueFloat);
-  console.log(tipAmountFloat);
-  console.log(billValueNum);
   totalPerPersonToFixed(totalPerPerson, total);
-  // puttingTotalPersonOnScreen(totalPerPerson, total);
 }
 
 function changingTypeOfTipAmountFloat(tipAmountFloat, numberOfPeople, bill, totalPerPerson, billValueNum, tipPercentage) {
   let tipAmountFloatNum = parseFloat(tipAmountFloat);
-  console.log(tipAmountFloatNum)
   calculatingTotalPerson(tipAmountFloatNum, numberOfPeople, bill, totalPerPerson, billValueNum, tipPercentage)
 }
 
@@ -85,7 +73,6 @@ function catnBeZeroOnScreen() {
   }
 }
 
-
 function puttingTipAmountOnScreen(tipAmountFloat, tipAmount) {
   tipAmount.innerHTML = `<p>$${tipAmountFloat}</p>`;
 }
@@ -98,86 +85,43 @@ function tipAmountToFixed(tipAmountPerson, tipAmount, tipPercentage) {
 
 function dividingTipPercentage(tipPercentage, numberOfPeople) {
   tipAmountPerson = tipPercentage / numberOfPeople.value;
-  console.log(tipAmountPerson);
-  console.log(tipPercentage);
-  // console.log(numberOfPeople.value);
   catnBeZeroOnScreen(numberOfPeople, cantBeZero);
   tipAmountToFixed(tipAmountPerson, tipAmount, tipPercentage);
 }
 
 function inputCalculatingTipPercentage(customInput, bill, tipPercentage, numberOfPeople) {
   tipPercentage = (customInput.value * bill.value) / 100;
-  console.log(tipPercentage);
   dividingTipPercentage(tipPercentage, numberOfPeople, tipPercentage);
 }
 
-// console.log(percent5.value);
 function btnCalculatingTipPercentage(activeBtn, bill, tipPercentage, numberOfPeople){
-  console.log(activeBtn.value);
   tipPercentage = (activeBtn.value * bill.value) / 100;
-  console.log(tipPercentage);
-  console.log(numberOfPeople);
   dividingTipPercentage(tipPercentage, numberOfPeople);
 }
 
 function handleButtonClick(activeBtn, customInput, bill, tipPercentage, numberOfPeople){
   return function(e) {
   
-  // console.log(i);
-  console.log(activeBtn.value);
-  // console.log(activeBtn[i]);
     if (customInput.value == "") {
-    // console.log("empty");
-    // console.log(button.value);
-    // console.log(customInput.value);
-
       btnCalculatingTipPercentage(activeBtn, bill, tipPercentage, numberOfPeople);
-      
     }
     else {
-      inputCalculatingTipPercentage(customInput, bill, tipPercentage, numberOfPeople)
-      // btnsPercentages.disabled = true;
+      inputCalculatingTipPercentage(customInput, bill, tipPercentage, numberOfPeople);
     }
   }
 }
 
-// function a2(btnsPercentages,customInput, bill){
-//     btnsPercentages.addEventListener('click', a1(btns, customInput, bill));
-// }
-
-// function a2(btnsPercentages,customInput, bill){
-//   btnsPercentages.forEach(btn => {
-//     btn.addEventListener('click', a1(btn, customInput, bill));
-//   });
-// }
-
 for (i; i < btnsPercentages.length; i++) {
-  // if (customInput.value == "") {
     if (customInput.value === "") {
     btnsPercentages[i].addEventListener('click', handleButtonClick(btnsPercentages[i], customInput, bill, tipPercentage, numberOfPeople));
-    console.log(btnsPercentages[i]);
-    } else {
-      btnsPercentages[i].disabled = true;
-    }
-    
-
-    // console.log(btnsPercentages[i]);
-  // }
+     } 
 }
 
-
-
 function handleSubmit(e) {
-    e.preventDefault() // prevent the default behaviour
-    // a2(btnsPercentages,customInput, bill)
+    e.preventDefault()
     if (customInput.value === "") {
       handleButtonClick(btnsPercentages, customInput, bill, numberOfPeople);
     }
   }
 
 form.addEventListener('submit', handleSubmit);
-
-
-console.log(bill.value);
-console.log(customInput.value);
-console.log("");
